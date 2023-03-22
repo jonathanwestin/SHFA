a791af92cc5f89959b3060c9fe1669697fa53b54
Tristan Bridge <tbridge360@gmail.com>
2023-03-22 13:21:03 +0100

    advanced search button changes and padding for panels

--- my-project/src/Views/Grid.vue
+++ my-project/src/Views/Grid.vue
@@ -4,15 +4,21 @@
   <h1 class="text-3xl m-8">SHFA</h1>
 <div class="w-3/4 mx-8 m-8">
     <div class="relative flex items-center">
-      <input type="text" id="search" name="search" placeholder="Search Image Database" class="border border-gray-400 py-3 pl-6 pr-10 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 w-full text-black">
+      <input type="text" id="search" name="search" placeholder="Search Image Database" class="border border-gray-400 py-3 pl-6 pr-10 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 w-full text-black drop-shadow-lg">
       <div class="absolute inset-y-0 right-0 flex items-center pr-3">
         <svg id="dropdown-toggle" class="w-5 h-5 text-gray-400 cursor-pointer" fill="currentColor" viewBox="0 0 20 20" v-on:click="showDropdown = !showDropdown">
           <path fill-rule="evenodd" d="M13.853 12.146a1 1 0 0 1-1.414 1.414l-1.147-1.147a4.5 4.5 0 1 1 1.414-1.414l1.147 1.147zm3.708 3.708a1 1 0 0 1-1.414 1.414l-3.8-3.8a6 6 0 1 1 1.414-1.414l3.8 3.8zM6.5 10a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" clip-rule="evenodd"></path>
         </svg>
-        <div id="dropdown" class="absolute z-10 right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl" v-if="showDropdown">
+        <div id="dropdown" class="absolute z-10 left-10 mt-2 py-2 w-48 bg-white rounded-md shadow-xl" v-if="showDropdown">
           <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white">Option 1</a>
           <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white">Option 2</a>
           <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white">Option 3</a>
+          <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white">Option 4</a>
+          <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white">Option 5</a>
+          <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white">Option 6</a>
+          <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white">Option 7</a>
+          <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-blue-500 hover:text-white">Option 8</a>
+
         </div>
       </div>
     </div>
@@ -27,7 +33,8 @@
        :class="{ 'w-full': showThreePanels, 'w-1/2': !showThreePanels }"
        @click="toggleThreePanels">
     <p>Second Panel</p>
-    <div class="max-w-lg mt-10 mx-6 lg:mx-auto">
+   <div class="max-w-lg mt-10 mx-6 lg:mx-auto">
+  <div class="p-6">
     <MasonryWall :items="items" :ssr-columns="1" :column-width="100" :gap="16">
       <template #default="{ item, index }">
         <div
@@ -38,13 +45,14 @@
         </div>
       </template>
     </MasonryWall>
-    <ul>
-      <li v-for="restaurant in restaurants.data" :key="restaurant.id">
-        <h2>{{ restaurant.attributes.name}}</h2>
-        <p>{{ restaurant.attributes.description }}</p>
-      </li>
-    </ul>
   </div>
+  <ul>
+    <li v-for="restaurant in restaurants.data" :key="restaurant.id">
+      <h2>{{ restaurant.attributes.name}}</h2>
+      <p>{{ restaurant.attributes.description }}</p>
+    </li>
+  </ul>
+</div>
   </div>
   <div class="flex-grow bg-gray-600 transition-all duration-500"
        :class="{ 'w-full': showThreePanels, 'w-1/2': showThreePanels, 'w-0': !showThreePanels }">
